6b54650325a5adaee14f495802deb1d79e7248af
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-05-05 20:37:46 -0300

    adding Footer component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "components/Footer";
 import { NavBar } from "components/NavBar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="container">
       Projeto SDS2 - DEV-SUPERIOR
     </div>
+    <Footer/>
     </>
   );
 }
