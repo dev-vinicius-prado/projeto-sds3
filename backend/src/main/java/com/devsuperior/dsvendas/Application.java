daf9ce5f3a4513bdd9056236ec1cc6aeb3c3d8f5
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-05-08 07:53:41 -0300

    Security config.

--- backend/src/main/java/com/devsuperior/dsvendas/Application.java
+++ backend/src/main/java/com/devsuperior/dsvendas/Application.java
@@ -2,12 +2,22 @@ package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+        System.out.println("hello world to Spring again...");
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "Hello rest access to Spring";
 	}
 
 }
